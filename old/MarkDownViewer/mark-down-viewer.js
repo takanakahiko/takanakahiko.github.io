83e92b589c77036abd0e2fc5b5f456dafb1a657b
takanakahiko <takanakahiko@gmail.com>
2019-02-24 17:43:23 +0900

    create nuxtx app

--- old/MarkDownViewer/mark-down-viewer.js
+++ old/MarkDownViewer/mark-down-viewer.js
@@ -1,53 +1,52 @@
-function getFile(){
-    var req = new XMLHttpRequest(); // XMLHttpRrequestオブジェクトを生成
-    req.responseType = "text";
-    var prop = GetQueryString();
-    var filename = (prop && prop["page"]) ? prop["page"] : "test.md";
-    req.open("get", "md/" + filename , true); // アクセスするファイルを指定
-    req.send(null);// HTTPリクエストの発行
-    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
-    req.onload = function(){
-      marked.setOptions({
-          langPrefix: ''
-      });
-      var md = marked(req.responseText);
-      document.querySelector('#sec_main').innerHTML = md;
-      hljs.initHighlightingOnLoad();
-      Array.from(
-        document.querySelectorAll("pre code"),
-        e => (function(){
-          e.classList.add("hljs");
-          return hljs.highlightBlock(e);
-        })()
-      );
-    }
+function getFile() {
+  const req = new XMLHttpRequest() // XMLHttpRrequestオブジェクトを生成
+  req.responseType = 'text'
+  const prop = GetQueryString()
+  const filename = prop && prop.page ? prop.page : 'test.md'
+  req.open('get', 'md/' + filename, true) // アクセスするファイルを指定
+  req.send(null) // HTTPリクエストの発行
+  // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
+  req.onload = function() {
+    marked.setOptions({
+      langPrefix: ''
+    })
+    const md = marked(req.responseText)
+    document.querySelector('#sec_main').innerHTML = md
+    hljs.initHighlightingOnLoad()
+    Array.from(document.querySelectorAll('pre code'), e =>
+      (function() {
+        e.classList.add('hljs')
+        return hljs.highlightBlock(e)
+      })()
+    )
+  }
 }
 
 /* thanks https://www.ipentec.com/document/document.aspx?page=javascript-get-parameter */
 function GetQueryString() {
-  if (1 < document.location.search.length) {
-    var query = document.location.search.substring(1);
-    var parameters = query.split('&');
-    var result = new Object();
-    for (var i = 0; i < parameters.length; i++) {
-      var element = parameters[i].split('=');
-      var paramName = decodeURIComponent(element[0]);
-      var paramValue = decodeURIComponent(element[1]);
-      result[paramName] = decodeURIComponent(paramValue);
+  if (document.location.search.length > 1) {
+    const query = document.location.search.substring(1)
+    const parameters = query.split('&')
+    const result = new Object()
+    for (let i = 0; i < parameters.length; i++) {
+      const element = parameters[i].split('=')
+      const paramName = decodeURIComponent(element[0])
+      const paramValue = decodeURIComponent(element[1])
+      result[paramName] = decodeURIComponent(paramValue)
     }
-    return result;
+    return result
   }
-  return null;
+  return null
 }
 
-function gitHubCallBack(json){
-  var parent = document.getElementById("sec_info");
-  for(var file of json.data){
-    var ele = document.createElement("a");
-    ele.setAttribute("href", "./index.html?page=" + file.name);
-    ele.innerHTML = file.name + "<br>";
-    parent.appendChild(ele);
+function gitHubCallBack(json) {
+  const parent = document.getElementById('sec_info')
+  for (const file of json.data) {
+    const ele = document.createElement('a')
+    ele.setAttribute('href', './index.html?page=' + file.name)
+    ele.innerHTML = file.name + '<br>'
+    parent.appendChild(ele)
   }
 }
 
-window.onload = getFile;
+window.onload = getFile
