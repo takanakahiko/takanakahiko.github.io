a94db63fd58d2657d02c7370eb877c0021dc113f
takanakahiko <takanakahiko@gmail.com>
2019-02-28 18:03:24 +0900

    using original token

--- .github/main.workflow
+++ .github/main.workflow
@@ -18,7 +18,7 @@ action "Deploy" {
   needs = "Build"
   uses = "docker://node:latest"
   runs = "node ./.github/scripts/gh-pages.js"
-  secrets = ["GITHUB_TOKEN"]
+  secrets = ["GITHUB_TOKEN_FOR_ACTION"]
   env = {
     GIT_NAME   = "takanakahiko"
     GIT_EMAILE = "takanakahiko@users.noreply.github.com"
--- .github/scripts/gh-pages.js
+++ .github/scripts/gh-pages.js
@@ -6,6 +6,7 @@ ghpages.publish('dist', {
         name: process.env.GIT_NAME,
         email: process.env.GIT_EMAIL
     },
-    repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/' + process.env.GITHUB_REPOSITORY + '.git',
-    dotfiles: true
+    repo: 'https://' + process.env.GITHUB_TOKEN_FOR_ACTION + '@github.com/' + process.env.GITHUB_REPOSITORY + '.git',
+    dotfiles: true,
+    silent: true
 }, function(err) { console.log(err) });
