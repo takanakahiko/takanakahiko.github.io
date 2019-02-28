0476c9b9d27be9fae601dc4ee3360f724ff4072e
takanakahiko <takanakahiko@gmail.com>
2019-02-28 17:17:56 +0900

    insert google anal

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,9 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+  plugins: [
+    { src: '~plugins/ga.js', ssr: false }
+  ],
 
   /*
   ** Nuxt.js modules
