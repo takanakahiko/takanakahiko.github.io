cc4c9c53c2c74c6432f1d6b2ffd4ec5640f3d5ba
takanakahiko <takanakahiko@gmail.com>
2019-02-28 16:44:35 +0900

    remove error handring in actions

--- .github/scripts/gh-pages.js
+++ .github/scripts/gh-pages.js
@@ -4,4 +4,4 @@ ghpages.publish('dist', {
     branch: 'master',
     repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/' + process.env.GITHUB_REPOSITORY + '.git',
     dotfiles: true
-}, function(err) { console.log(err) });
+});
