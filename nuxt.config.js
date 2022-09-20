dcfb4e09be8e832ca66d7e801da432b4dcb1f37a
takanakahiko <takanakahiko@gmail.com>
2022-09-21 00:57:18 +0900

    disable eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,18 +77,6 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-          options: {
-            fix: true
-          }
-        })
-      }
     },
     transpile: [
       'three/examples/jsm/controls/OrbitControls.js'
