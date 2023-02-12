8cc08331ff7cc923b45c720579eba6877549bb88
takanakahiko <takanakahiko@users.noreply.github.com>
2023-02-12 10:49:14 +0000

    deploy: 3dfdbf6599439352d0511b3f4f60f63858c460d4

--- _nuxt/app.config.7e3cf641.js
+++
@@ -1,1 +0,0 @@
-import{m as u,E as s}from"./entry.ddf7497b.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ _nuxt/app.config.9cc9e802.js
@@ -0,0 +1,1 @@
+import{q as u,E as s}from"./entry.8d5d983c.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
